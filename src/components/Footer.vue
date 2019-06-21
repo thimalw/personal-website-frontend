e293aec0825b908485e673313dd8f430cd48047f
Thimal Wickremage <thimalmadushanka@gmail.com>
2019-06-21 18:46:41 +1000

    add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+    <footer class="site-footer">
+        <span class="footer-details">&copy; Thimal Wickremage</span>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style lang="css">
+.site-footer {
+    text-align: center;
+    color: #aaa;
+    font-size: 12px;
+    padding: 50px 15px;
+    border-top: 1px solid #eee;
+}
+</style>
